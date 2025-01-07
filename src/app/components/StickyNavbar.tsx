5f5017bf01fdd7a2223754c9bfa13082e0eacba7
kersolo <loick.ferelloc@gmail.com>
2025-01-07 17:15:25 +0100

    fix navbar mobile

--- src/app/components/StickyNavbar.tsx
+++ src/app/components/StickyNavbar.tsx
@@ -42,7 +42,10 @@ export function StickyNavbar() {
     <>
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 border-none bg-blue-gray-900">
         <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-          <Link onClick={() => setOpenNav(isMobile && !openNav)} href={"/"}>
+          <Link
+            onClick={() => openNav && setOpenNav(isMobile && !openNav)}
+            href={"/"}
+          >
             <Image
               src={logo_objectifLarge}
               priority={true}
